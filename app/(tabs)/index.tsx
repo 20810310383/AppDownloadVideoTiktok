082e86800a8245f8dded064e03f0c635cc486784
Do Khac Tu <dokhactu1822@gmail.com>
2025-09-09 14:50:15 +0700

    v26

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -196,9 +196,11 @@ export default function HomeScreen() {
 
       // Nếu là lần thứ 3 → show QC
       if (newCount % 3 === 0) {
-        InterstitialQC.show(() => {
-          saveFile(href, name, kind);
-        });
+        // InterstitialQC.show(() => {
+        //   saveFile(href, name, kind);
+        // });
+
+        saveFile(href, name, kind);
       } else {
         // tải thẳng
         saveFile(href, name, kind);
@@ -240,7 +242,7 @@ export default function HomeScreen() {
                 ) : (
                   <View style={styles.logoDot} />
                 )}
-                <Text style={styles.brandLeft}>Vid</Text>
+                <Text style={styles.brandLeft}>Tikpro</Text>
                 <Text style={styles.brandRight}>Down</Text>
               </View>
               <View style={styles.topIcons}>
@@ -457,7 +459,7 @@ export default function HomeScreen() {
             )}
 
             {/* Banner Ads */}
-            <BannerQC />
+            {/* <BannerQC /> */}
 
             {/* tagline */}
             <View style={styles.footer}>
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 // app/index.tsx
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   View,
   Text,
@@ -8,23 +8,41 @@ import {
   ImageBackground,
 } from "react-native";
 import { router } from "expo-router";
+import LottieView from "lottie-react-native";
 
 export default function WelcomeScreen() {
+  const [displayedText, setDisplayedText] = useState("");
+  const animationRef = useRef<LottieView>(null);
+
+  const fullText = "Tikpro Video Downloader\nNo watermark";
+
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setDisplayedText(fullText.slice(0, i + 1));
+      i++;
+      if (i === fullText.length) clearInterval(interval);
+    }, 50);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require("../assets/images/mancho.png")}
+        source={require("../assets/images/manchoo.png")}
         style={styles.bg}
         resizeMode="cover"
       />
       <View style={styles.overlay} />
 
       <View style={styles.wrap}>
+        <Text style={styles.typewriterText}>{displayedText}</Text>
         <TouchableOpacity
           style={styles.btn}
           activeOpacity={0.9}
           onPress={() => router.replace("/(tabs)")}
         >
+          {/* nút bắt đầu */}
           <Text style={styles.btnText}>Bắt đầu</Text>
         </TouchableOpacity>
       </View>
@@ -39,6 +57,16 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     backgroundColor: "rgba(0,0,0,0.25)",
   },
+
+  btnText: { color: "#fff", fontWeight: "800", fontSize: 20 },
+
+  typewriterText: {
+    fontSize: 31,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+    marginBottom: 80, // khoảng cách so với nút
+  },
   wrap: {
     flex: 1,
     alignItems: "center",
@@ -49,12 +77,11 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: "rgba(239,68,68,0.95)",
     paddingHorizontal: 28,
-    height: 46,
+    paddingVertical: 12,
     borderRadius: 24,
     alignItems: "center",
     justifyContent: "center",
-    minWidth: 180,
+    minWidth: 200,
     elevation: 1,
   },
-  btnText: { color: "#fff", fontWeight: "800", fontSize: 20 },
 });
